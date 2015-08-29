a70c942624db7309ab532f874b23ee8369ac1cbd
Simon Yang <lokisnake@gmail.com>
2015-08-28 23:57:27 -0700

    Adding new implementation of histogram based on input from srhb

--- Golf.hs
+++ Golf.hs
@@ -1,6 +1,8 @@
 module Golf where
 
 import Data.List.Split
+import Data.List
+import qualified Data.Map.Lazy as Map
 
 skips :: [a] -> [[a]]
 skips xs = map (\i -> concatMap (drop (i - 1)) (chunksOf i xs)) [1..(length xs)]
@@ -17,22 +19,39 @@ isMaxima (a, b, c) = (a < b) && (b > c)
 histogram :: [Integer] -> String
 histogram = addBottom . genHistogram . countNumbers
 
-countNumbers :: [Integer] -> [Integer]
-countNumbers = foldr insertNumber $ replicate 10 0
+countNumbers :: [Integer] -> Map.Map Integer Int
+countNumbers = Map.fromList . map (\x -> (head x, length x)) . group . sort
 
-insertNumber :: Integer -> [Integer] -> [Integer]
-insertNumber _ [] = undefined
-insertNumber 0 (x:xs) = (x+1):xs
-insertNumber i (x:xs) = x : insertNumber (i - 1) xs
+genHistogram :: Map.Map Integer Int -> String
+genHistogram m = concatMap (genRow m) rows
+  where rows = reverse [1..maxCount]
+        maxCount = Map.foldr max 0 m
 
-genHistogram :: [Integer] -> String
-genHistogram xs = concatMap (genRow xs) rows
-  where rows = reverse [1..(maximum xs)]
+genRow :: Map.Map Integer Int -> Int -> String
+genRow m row = map s [0..9] ++ "\n"
+  where s n = if val n >= row then '*' else ' '
+        val n = Map.findWithDefault 0 n m
 
-genRow :: [Integer] -> Integer -> String
-genRow [] _ = "\n"
-genRow (x:xs) row = s : genRow xs row
-  where s = if x >= row then '*' else ' '
+-- countNumbers :: [Integer] -> [Integer]
+-- countNumbers = foldr insertNumber $ replicate 10 0
+-- countNumbers xs = map (fromIntegral . subtract 1 . length) . group . sort $ [0..9] ++ xs
+
+-- countNumbers' :: [Integer] -> [(Integer, Integer)]
+-- countNumbers' = map (\x -> (head x, fromIntegral $ length x)) . group . sort
+
+-- insertNumber :: Integer -> [Integer] -> [Integer]
+-- insertNumber _ [] = undefined
+-- insertNumber 0 (x:xs) = (x+1):xs
+-- insertNumber i (x:xs) = x : insertNumber (i - 1) xs
+--
+-- genHistogram :: [Integer] -> String
+-- genHistogram xs = concatMap (genRow xs) rows
+--   where rows = reverse [1..(maximum xs)]
+--
+-- genRow :: [Integer] -> Integer -> String
+-- genRow [] _ = "\n"
+-- genRow (x:xs) row = s : genRow xs row
+--   where s = if x >= row then '*' else ' '
 
 addBottom :: String -> String
 addBottom = (++bottom)
